0ae5ef4c97be6ad255c9adafac2a3a10f08c31d6
Likitha Donta <73773198+Santaforgotmyaddress@users.noreply.github.com>
2023-09-20 20:55:27 +0530

    Add files via upload

---
+++ #FizzBuzzProblem.py
@@ -0,0 +1,11 @@
+#FizzBuzzProblem
+
+for i in range(1,100):
+    if (i % 5 == 0 and i % 3 == 0):
+        print("fizz buzz")
+    elif (i % 3 == 0):
+        print("fizz")
+    elif (i % 5 == 0):
+        print("buzz")
+    else:
+        print(i) 
